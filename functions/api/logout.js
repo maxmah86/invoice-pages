fef7566e63ab017c9aeb3e99b1d792f5fe9c1bf2
maxmah86 <132794896+maxmah86@users.noreply.github.com>
2026-01-10 22:11:01 +0800

    Delete functions/api directory

--- functions/api/logout.js
+++
@@ -1,9 +0,0 @@
-export async function onRequest() {
-  return new Response(null, {
-    status: 302,
-    headers: {
-      "Set-Cookie": "session=; Path=/; Max-Age=0",
-      "Location": "/login.html"
-    }
-  });
-}
