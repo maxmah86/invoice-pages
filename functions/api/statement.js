15ee64ef6f4b8051f2120cd656dfb67fd785e38c
maxmah86 <132794896+maxmah86@users.noreply.github.com>
2026-02-04 09:53:35 +0800

    Update statement.js

--- functions/api/statement.js
+++ functions/api/statement.js
@@ -64,7 +64,7 @@ export async function onRequest({ request, env }) {
      * =============================== */
     if (status) {
       sql += " AND status LIKE ?";
-      binds.push(`%${status}%`);
+      binds.push(`${status}`);
     }
 
   if (from) {
